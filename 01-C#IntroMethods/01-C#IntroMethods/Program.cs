b8c895495e1eca02cfab15cc44d17dca0ab67c4c
apa202ibort <ibrahimrs-apa202@code.edu.az>
2026-03-18 20:59:23 +0400

    task1

---
+++ 01-C#IntroMethods/01-C#IntroMethods/Program.cs
@@ -0,0 +1,31 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine(Topla(5, 1));
+        Console.WriteLine(Cix(5, 5));
+        Console.WriteLine(Vur(5, 7));
+        Console.WriteLine(Bol(6, 3));
+    }
+
+    static int Topla(int a, int b)
+    {
+        return a + b;
+    }
+
+    static int Cix(int a, int b)
+    {
+        return a - b;
+    }
+
+    static int Vur(int a, int b)
+    {
+        return a * b;
+    }
+
+    static double Bol(int a, int b)
+    {
+        return (double)a / b;
+    }
+}
+
